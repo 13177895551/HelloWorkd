378de218bc8fe742b5a22062d84eea2e2bf3257e
Mingxin <v-minz@microsoft.com>
2019-03-13 15:52:43 +0800

    version02

---
+++ Hello World/app/src/main/java/com/examplev_minz/v_minz/helloworld/HelloWorldMainActivity.java
@@ -0,0 +1,13 @@
+package com.examplev_minz.v_minz.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelloWorldMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_world_main);
+    }
+}
