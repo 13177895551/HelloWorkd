b97ce98bef980614d9fe5b23671404d943ccebd7
Mingxin <v-minz@microsoft.com>
2019-03-22 17:11:29 +0800

    map

---
+++ Hello World/BuildScripts/TeamService/无标题1.ps1
@@ -0,0 +1,54 @@
+﻿<ul class="nav nav-tabs vertical-tabs">
+<li class="stack">
+<div id="anchor" data-title="App" data-level="1" data-href="https://rink.hockeyapp.net/manage/apps/776525"></div>
+<script>
+  try {
+    sessionStorage.setItem("level" + "1", JSON.stringify({ href: "https://rink.hockeyapp.net/manage/apps/776525", title: "App" }) );
+  
+    for (index = 1 + 1; index <= 10; index++) {
+      sessionStorage.removeItem("level" + index);
+    }
+  }
+  catch ( err ) {
+  }
+</script>
+<script>
+  try {
+    sessionStorage.setItem("crashes", JSON.stringify({ href: "https://rink.hockeyapp.net/manage/apps/776525", title: "App" }) );
+  }
+  catch ( err ) {
+  }
+</script>
+<script>
+  try {
+    sessionStorage.setItem("feedback", JSON.stringify({ href: "https://rink.hockeyapp.net/manage/apps/776525", title: "App" }) );
+  }
+  catch ( err ) {
+  }
+</script>
+<script>
+  info = JSON.parse(sessionStorage.getItem("level0"));
+  if (info !== null) {
+    document.write("<a id='up' href='" + info["href"] + "'></a><span>App</span>");
+  }
+  else if (true) {
+    document.write("<a id='up' href='/manage/dashboard'></a><span>App</span>");
+  }
+  else {
+    document.write("<a name='stack'></a><span>App</span>");
+  }
+</script><a id="up" href="/manage/dashboard"></a><span>App</span>
+</li>
+
+
+<li class="active "><a href="/manage/apps/776525">Overview</a></li>
+<li class=" "><a href="/manage/apps/776525/app_versions">Versions<span class="counter ">220</span></a></li>
+<li class=" "><a href="/manage/apps/776525/crash_reasons">Crashes<span class="counter ">8053</span></a></li>
+<li class=" "><a href="/manage/apps/776525/app_events">Events</a></li>
+<li class=" "><a href="/manage/apps/776525/feedback">Feedback<span class="counter zero">0</span></a></li>
+<li class=" "><a href="/manage/apps/776525/app_users">Users<span class="counter ">1336</span></a></li>
+<li class="hideable pull-right">
+<a href="/manage/apps/776525/activities">Timeline</a>
+</li>
+
+</ul>
